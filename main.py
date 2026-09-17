4a804b59f5b336e1c4e12809dc813281fe8bb33f
khalidmahmud999 <khalidmahmud9998@gmail.com>
2026-09-17 17:34:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.camera import Camera
-from kivy.uix.button import Button
-from kivy.uix.label import Label
+from kivy.uix.boxlayout \
+import BoxLayout
+from kivy.uix.camera \
+import Camera
+from kivy.uix.button \
+import Button
+from kivy.uix.label \
+import Label
 import time
 
 class Cam(BoxLayout):
  def __init__(self,**k):
   super().__init__(**k)
-  self.orientation='vertical'
+  self.orientation=\
+  'vertical'
   self.t=Label(
    text='Pixel 11 Pro',
    size_hint=(1,.1)
   )
-  self.add_widget
+  self.add_widget(self
