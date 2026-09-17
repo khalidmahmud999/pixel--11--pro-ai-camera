e918cb3c963a6e8d53f9373486ab9d05caa6fc27
khalidmahmud999 <khalidmahmud9998@gmail.com>
2026-09-17 17:10:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from kivy.uix.camera import Camera
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 import time
+import os
 
 class Pixel11ProCamera(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(orientation='vertical', **kwargs)
         
-        # Pixel 11 Pro 4K Camera
-        self.cam = Camera(play=True, resolution=(1920, 1080), index=0)
-        self.add_widget(self.cam)
-
-        # Info Label - S26 Ultra Style
-        self.info = Label(text="Pixel 11 Pro | 4K 60FPS | AI HDR | OIS ON", size_hint=(1, 0.15))
+        # Top Bar - S26 Ultra Style
+        self.info = Label(
+            text="Pixel 11 Pro | 4K 60FPS | AI HDR+ ON | 200MP",
+            size_hint=(1, 0.1),
+            font_size='14sp'
+        )
         self.add_widget(self.info)
 
-        #
+        # Main 4K Camera
+        self.cam = Camera(play=True, resolution=(1920, 1080), index=0)
+       
