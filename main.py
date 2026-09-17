9265f8f9b843225d8d489cbd1a1f00ad24ccc2ce
khalidmahmud999 <khalidmahmud9998@gmail.com>
2026-09-17 17:24:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+ 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
@@ -6,15 +7,12 @@ from kivy.uix.label import Label
 import time
 
 class Pixel11ProCamera(BoxLayout):
-    def __init__(self, **kw):
-        super().__init__()
-        self.orientation='vertical'
-
-        self.info=Label(
-            text='Pixel 11 Pro | 4K',
-            size_hint=(1,.1)
-        )
-        self.add_widget(self.info)
-
-        self.cam=
-           
+ def __init__(self,**kw):
+  super().__init__()
+  self.orientation='vertical'
+  self.info=Label(
+   text='Pixel 11 Pro | 4K',
+   size_hint=(1,.1)
+  )
+  self.add_widget(self.info)
+  self.cam
